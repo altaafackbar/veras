a4681ac7688c37f840236adb14105e52acd3f79c
altaafackbar <altaafackbar@hotmail.com>
2022-06-20 16:14:12 -0600

    captcha and checkboxes

--- src/pages/FormPages/Candidates.js
+++ src/pages/FormPages/Candidates.js
@@ -7,12 +7,29 @@ import Stack from '@mui/material/Stack';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 
-export class Candidates extends Component {
-    
-  render() {
+export default function Candidates() {
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  }
 
     const { values, handleChange } = this.props;
+    const candidates = this.props.candidates;
     return (
         <div style={{flexDirection: 'flex-column', justifyContent: 'center', alignItems: 'center'}}>
         <section class="electionHeader">
@@ -33,18 +50,6 @@ export class Candidates extends Component {
 
                 <p className='labels'>Choose your candidates</p>
   
-                <div class="input">
-                  <TextField
-                    placeholder="Choose your candidates"
-                    onChange={handleChange('email')}
-                    defaultValue={values.email}
-                    margin="normal"
-                    fullWidth
-                    id="outlined-basic"
-                    variant="outlined"
-                    
-                  />
-                  </div>
                 
             </div>
             
@@ -53,6 +58,4 @@ export class Candidates extends Component {
     </div>
     )
   }
-}
 
-export default Candidates
--- src/pages/FormPages/FormPersonalInfo.js
+++ src/pages/FormPages/FormPersonalInfo.js
@@ -7,16 +7,40 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@mui/material/Checkbox';
+import { IconContext } from 'react-icons'
+import * as AiIcons from 'react-icons/ai'
+import { createTheme } from '@mui/material/styles';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export class FormPersonalInfo extends Component {
 
   continue = e => {
+    console.log(this.props)
     e.preventDefault();
     this.props.nextStep();
   };
 
-  render() {
+  onChange = value => {
+    console.log('Captcha value:', value);
+  }
 
+  render() {
+    const theme = createTheme({
+      status: {
+        danger: '#e53e3e',
+      },
+      palette: {
+        primary: {
+          main: '#0971f1',
+          darker: '#053e85',
+        },
+        veras: {
+          main: '#766cce',
+          darker: '#5b51ad',
+        },
+      },
+    });
     const { values, handleChange } = this.props;
     return (
       <div style={{flexDirection: 'flex-column', justifyContent: 'center', alignItems: 'center'}}>
@@ -47,50 +71,58 @@ export class FormPersonalInfo extends Component {
                     fullWidth
                     id="outlined-basic"
                     variant="outlined"
+                    className='inputField'
                     
                   />
               </div>
               <p className='labels'>Date of Birth - Year/Month/Day</p>
 
               <div class="input">
-                <TextField
-                  placeholder="ISRN"
-                  onChange={handleChange('email')}
-                  defaultValue={values.email}
-                  margin="normal"
-                  fullWidth
-                  id="outlined-basic"
-                  variant="outlined"
-                  
-                  
-                />
-                </div>
-              <div class="input">
-                <TextField
-                  id="outlined-basic"
-                  type="date"
-                  defaultValue="2017-05-24"
-                  fullWidth
-                  sx={{ width: 220 }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                />
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DatePicker
-                    label="Basic example"
-                    value={values.email}
-                    onChange={handleChange('email')}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
+              <TextField
+                placeholder="ISRN"
+                type='date'
+                onChange={handleChange('email')}
+                defaultValue={values.email}
+                margin="normal"
+                fullWidth
+                id="outlined-basic"
+                variant="outlined"
+                className='inputField'
+                
+              />
               </div>
               <br />
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={this.continue}
-            >Continue</Button>
+              <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                <Checkbox
+                  
+                  defaultChecked
+                  sx={{ '& .MuiSvgIcon-root': { fontSize: 35 } }}
+                />
+                <p className='checkboxFont'>I have read and accepted our policies</p>
+              </div>
+
+
+              <div className="App">
+                <ReCAPTCHA
+                  sitekey="6LfLyYcgAAAAAJfJBFpEUQctocaRiRGoCoy0-lg7"
+                  onChange={this.onChange}
+                />
+              </div>
+              <br />
+              <div class="input">
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={this.continue}
+                fullWidth
+                className='navButton'
+              >Continue 
+                <IconContext.Provider value={{ style: {fontSize: '25', color: '#520e9c'}}}>
+                    <AiIcons.AiOutlineArrowRight/>
+                </IconContext.Provider>
+              </Button>
+                </div>
+
           </div>
 
 
